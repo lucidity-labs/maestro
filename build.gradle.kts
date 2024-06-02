ead9fc0ed3b54c9610546ff55630baa824fab3e2
lspahija <lspahijaoec@gmail.com>
2024-06-02 03:00:53 -0500

    added serdes

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     implementation("org.postgresql:postgresql:42.7.3")
     implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ src/main/java/org/example/engine/internal/Json.java
@@ -0,0 +1,31 @@
+package org.example.engine.internal;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Json {
+
+    private static final Logger logger = Logger.getLogger(Json.class.getName());
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <T> String serialize(T object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            logger.log(Level.SEVERE, "Error serializing object: " + object, e);
+            return null;
+        }
+    }
+
+    public static <T> T deserialize(String jsonString, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(jsonString, clazz);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error deserializing string: " + jsonString, e);
+            return null;
+        }
+    }
+}
