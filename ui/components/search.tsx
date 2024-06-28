b0ee9bcd092d7c469739af3a69edbf3a53c67e6d
lspahija <lspahijaoec@gmail.com>
2024-06-28 20:26:30 +0200

    dashboard skeleton

---
+++ ui/components/search.tsx
@@ -0,0 +1,13 @@
+import { Input } from "./input"
+
+export function Search() {
+  return (
+    <div>
+      <Input
+        type="search"
+        placeholder="Search..."
+        className="md:w-[100px] lg:w-[300px]"
+      />
+    </div>
+  )
+}
