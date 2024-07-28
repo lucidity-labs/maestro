92b96dec6405267d57b1ad67ee1f37ad3748ec84
lspahija <lspahijaoec@gmail.com>
2024-07-28 01:56:03 -0500

    passing workflow id in path params

--- example/src/main/java/org/example/example/api/Controller.java
+++ example/src/main/java/org/example/example/api/Controller.java
@@ -6,6 +6,7 @@ import org.example.example.workflow.OrderWorkflow;
 import org.example.example.workflow.OrderWorkflowImpl;
 import org.example.example.workflow.OrderInput;
 import org.example.example.workflow.OrderFinalized;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +17,16 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("/api")
 public class Controller {
 
-    @PostMapping("/execute")
-    public OrderFinalized executeWorkflow() throws ExecutionException, InterruptedException {
-        OrderWorkflow workflow = Maestro.newWorkflow(OrderWorkflowImpl.class, new WorkflowOptions("ac1ade8e-1b7b-4784-a15c-724403a77b5b"));
+    @PostMapping("/execute/{workflowId}")
+    public OrderFinalized executeWorkflow(@PathVariable String workflowId) throws ExecutionException, InterruptedException {
+        OrderWorkflow workflow = Maestro.newWorkflow(OrderWorkflowImpl.class, new WorkflowOptions(workflowId));
 
         return workflow.submitOrder(new OrderInput("someInput"));
     }
 
-    @PostMapping("/signal")
-    public void signalWorkflow() {
-        OrderWorkflow workflow = Maestro.newWorkflow(OrderWorkflowImpl.class, new WorkflowOptions("ac1ade8e-1b7b-4784-a15c-724403a77b5b"));
+    @PostMapping("/signal/{workflowId}")
+    public void signalWorkflow(@PathVariable String workflowId) {
+        OrderWorkflow workflow = Maestro.newWorkflow(OrderWorkflowImpl.class, new WorkflowOptions(workflowId));
 
         workflow.confirmShipped(new OrderInput("someSignalInput"));
     }
