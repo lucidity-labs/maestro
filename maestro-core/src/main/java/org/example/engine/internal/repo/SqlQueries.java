430b39ceae41073a32706a24d18a1af37eb29923
lspahija <lspahijaoec@gmail.com>
2024-08-01 15:36:42 -0500

    cleaned up queries

--- maestro-core/src/main/java/org/example/engine/internal/repo/SqlQueries.java
+++ maestro-core/src/main/java/org/example/engine/internal/repo/SqlQueries.java
@@ -2,50 +2,74 @@ package org.example.engine.internal.repo;
 
 public class SqlQueries {
     //language=SQL
-    public static final String INSERT_EVENT = "INSERT INTO event " +
-            "(id, workflow_id, correlation_number, sequence_number, category, class_name, function_name, data, status) " +
-            "VALUES (?, ?, ?::bigint, ?::bigint, ?::category, ?, ?, ?::json, ?::status)";
+    public static final String INSERT_EVENT = """
+            INSERT INTO event
+                        (id, workflow_id, correlation_number, sequence_number, category, class_name, function_name, data, status)
+                        VALUES (?, ?, ?::bigint, ?::bigint, ?::category, ?, ?, ?::json, ?::status)
+            """;
 
     //language=SQL
-    public static final String SELECT_EVENT = "SELECT id, workflow_id, correlation_number, sequence_number, category, class_name, function_name, data, status, timestamp " +
-            "FROM event WHERE workflow_id = ? AND category = ?::category AND status = ?::status";
+    public static final String SELECT_EVENT = """
+            SELECT id, workflow_id, correlation_number, sequence_number, category, class_name, function_name, data, status, timestamp
+                        FROM event WHERE workflow_id = ? AND category = ?::category AND status = ?::status
+            """;
 
     //language=SQL
-    public static final String SELECT_EVENT_BY_CORRELATION_NO = "SELECT id, workflow_id, correlation_number, sequence_number, category, class_name, function_name, data, status, timestamp " +
-            "FROM event WHERE workflow_id = ? AND correlation_number= ?::bigint AND status = ?::status";
+    public static final String SELECT_EVENT_BY_CORRELATION_NO = """
+            SELECT id, workflow_id, correlation_number, sequence_number, category, class_name, function_name, data, status, timestamp
+                        FROM event WHERE workflow_id = ? AND correlation_number= ?::bigint AND status = ?::status
+            """;
 
     //language=SQL
-    public static final String SELECT_WORKFLOWS = "SELECT id, workflow_id, correlation_number, sequence_number, category, class_name, function_name, data, status, timestamp " +
-            "FROM event WHERE category = 'WORKFLOW'";
+    public static final String SELECT_WORKFLOWS = """
+            SELECT started.workflow_id,
+                   started.class_name,
+                   started.function_name,
+                   started.timestamp   AS start_timestamp,
+                   completed.timestamp AS end_timestamp,
+                   started.data AS input,
+                   completed.data AS output
+            FROM event AS started LEFT JOIN event AS completed
+                 ON started.workflow_id = completed.workflow_id
+                     AND completed.category = 'WORKFLOW'
+                     AND completed.status = 'COMPLETED'
+            WHERE started.category = 'WORKFLOW'
+              AND started.status = 'STARTED';
+            """;
 
     //language=SQL
-    public static final String SELECT_EVENTS_BY_WORKFLOW_ID = "SELECT id, workflow_id, correlation_number, sequence_number, category, class_name, function_name, data, status, timestamp " +
-            "FROM event WHERE workflow_id = ?";
+    public static final String SELECT_EVENTS_BY_WORKFLOW_ID = """
+            SELECT id, workflow_id, correlation_number, sequence_number, category, class_name, function_name, data, status, timestamp
+                        FROM event WHERE workflow_id = ?
+            """;
 
     //language=SQL
     public static final String MAX_SEQUENCE_NUMBER = "SELECT max(sequence_number) FROM event WHERE workflow_id = ?";
 
     //language=SQL
-    public static final String SELECT_SIGNALS = "SELECT * FROM event " +
-            "WHERE workflow_id = ? " +
-            "AND category = 'SIGNAL' " +
-            "AND sequence_number > (" +
-            "SELECT COALESCE(max(sequence_number), 0) " +
-            "FROM event " +
-            "WHERE status = 'COMPLETED' " +
-            "AND workflow_id = ? " +
-            "AND sequence_number < ?) " +
-            "AND sequence_number < ?";
+    public static final String SELECT_SIGNALS = """
+            SELECT *
+            FROM event
+            WHERE workflow_id = ?
+              AND category = 'SIGNAL'
+              AND sequence_number > (SELECT COALESCE(max(sequence_number), 0)
+                                     FROM event
+                                     WHERE status = 'COMPLETED'
+                                       AND workflow_id = ?
+                                       AND sequence_number < ?)
+              AND sequence_number < ?
+            """;
 
     //language=SQL
-    public static final String SELECT_ABANDONED_WORKFLOWS =
-            "SELECT DISTINCT ON (e1.workflow_id) e1.* " +
-                    "FROM event e1 " +
-                    "WHERE e1.category = 'WORKFLOW' AND e1.status = 'STARTED' " +
-                    "  AND NOT EXISTS ( " +
-                    "    SELECT 1 " +
-                    "    FROM event e2 " +
-                    "    WHERE e1.workflow_id = e2.workflow_id " +
-                    "      AND ((e2.category = 'WORKFLOW' AND e2.status = 'COMPLETED') OR e2.timestamp > CURRENT_TIMESTAMP - INTERVAL '1 hour') " + // TODO: make this interval customizable
-                    ");";
+    public static final String SELECT_ABANDONED_WORKFLOWS = """
+            SELECT DISTINCT ON (e1.workflow_id) e1.*
+            FROM event e1
+            WHERE e1.category = 'WORKFLOW'
+              AND e1.status = 'STARTED'
+              AND NOT EXISTS (SELECT 1
+                              FROM event e2
+                              WHERE e1.workflow_id = e2.workflow_id
+                                AND ((e2.category = 'WORKFLOW' AND e2.status = 'COMPLETED') OR
+                                     e2.timestamp > CURRENT_TIMESTAMP - INTERVAL '1 hour')); -- TODO: make this interval customizable
+            """;
 }
